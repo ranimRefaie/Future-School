13d29545182fed7de84dfef1851f647da2ca3fdd
ranimRefaie <ranemalre@gmail.com>
2024-08-25 21:33:03 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import NavBar from "./Components/NavBar/NavBar";
+import bg_about from "./assets/bg-about.png";
+import Hero from "./Components/Hero/Hero";
+function App() {
+  return (
+    <div>
+      <NavBar />
+
+      <Hero />
+    </div>
+  );
+}
+
+export default App;
